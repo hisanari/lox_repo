d86e1cea124ab97775ef7c5edc1aab1b4cf8464f
hisanari <hisanari.1987@gmail.com>
2023-12-10 13:20:05 +0900

    add runFile

---
+++ src/lox/Lox.java
@@ -0,0 +1,22 @@
+package lox;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Path.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+}
