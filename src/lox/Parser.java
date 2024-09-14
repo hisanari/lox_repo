76e1b668ad01b5b3ddbed10bd1efc5589c140973
hisanari <hisanari.1987@gmail.com>
2024-09-14 16:19:08 +0900

    add superclass.

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -87,6 +87,13 @@ public class Parser {
 
     private Stmt classDeclaration() {
         Token name = consume(IDENTIFIER, "Expect class name.");
+
+        Expr.Variable superclass = null;
+        if (match(LESS)) {
+            consume(IDENTIFIER, "Expect class name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         consume(LEFT_BRACE, "Expect { before class body.");
 
         List<Stmt.Function> methods = new ArrayList<>();
@@ -96,7 +103,7 @@ public class Parser {
 
         consume(RIGHT_BRACE, "Expect } after class body.");
 
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt statement() {
@@ -368,6 +375,13 @@ public class Parser {
 
         if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
+        }
+
+        if (match(SUPER)) {
+            Token keyword = previous();
+            consume(DOT, "Expect ',' after 'super'.");
+            Token method = consume(IDENTIFIER, "Expect superclass method nema.");
+            return new Expr.Super(keyword, method);
         }
 
         if (match(THIS)) return new Expr.This(previous());
